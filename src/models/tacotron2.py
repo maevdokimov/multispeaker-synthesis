6398cfcf9e100fb45287e70e664a684b57aec259
maevdokimov <43167887+maevdokimov@users.noreply.github.com>
2022-02-09 16:54:39 +0300

    Tacotron 2 with correct logging (#4)
    * fix: :bug: incorrect tensorboard logging
    
    * fix: :bug: fix incorrect labels in tacotron config
    
    * feat: :sparkles: patched tacotron2 with correct tensorboard logging
    
    * feat: :sparkles: dataset inspection

---
+++ src/models/tacotron2.py
@@ -0,0 +1,35 @@
+import torch
+from nemo.collections.tts.models.tacotron2 import Tacotron2Model
+from pytorch_lightning.loggers import LoggerCollection, TensorBoardLogger
+
+from src.utils.helpers import tacotron2_log_to_tb_func
+
+
+class Tacotron2Model(Tacotron2Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def validation_epoch_end(self, outputs):
+        if self.logger is not None and self.logger.experiment is not None:
+            tb_logger = self.logger.experiment
+            if isinstance(self.logger, LoggerCollection):
+                for logger in self.logger:
+                    if isinstance(logger, TensorBoardLogger):
+                        tb_logger = logger.experiment
+                        break
+
+            if self.global_step != 0:
+                tacotron2_log_to_tb_func(
+                    tb_logger,
+                    outputs[0].values(),
+                    self.global_step,
+                    tag="val",
+                    log_images=True,
+                    add_audio=True,
+                    sr=self.cfg.preprocessor.sample_rate,
+                    n_fft=self.cfg.preprocessor.n_fft,
+                    n_mels=self.cfg.preprocessor.nfilt,
+                )
+
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()  # This reduces across batches, not workers!
+        self.log("val_loss", avg_loss)
---
+++ src/utils/inspect_dataset.py
@@ -0,0 +1,65 @@
+import argparse
+import json
+from collections import Counter
+from pathlib import Path
+
+import numpy as np
+
+
+def inspect_dataset(file_path: Path):
+    chars, chars_normalized, chars_no_preprocessing = Counter(), Counter(), Counter()
+    file_cnt = 0
+    text_cnt = 0
+    text_normalized_cnt = 0
+    text_no_preprocessing_cnt = 0
+    durations = []
+
+    with open(file_path, "r") as file:
+        for line in file:
+            d = json.loads(line)
+            file_cnt += 1
+            durations.append(d["duration"])
+
+            if "text" in d.keys():
+                chars.update(d["text"])
+                text_cnt += 1
+            if "text_normalized" in d.keys():
+                chars_normalized.update(d["text_normalized"])
+                text_normalized_cnt += 1
+            if "text_no_preprocessing" in d.keys():
+                chars_no_preprocessing.update(d["text_no_preprocessing"])
+                text_no_preprocessing_cnt += 1
+
+    print(f"Num lines: {file_cnt}")
+    print("----------Text characters----------")
+    print(f"Num texts: {text_cnt}")
+    print(chars)
+    print("----------Text characters----------")
+    print(f"Num normalized_texts: {text_normalized_cnt}")
+    print(chars_normalized)
+    print("----------Text characters----------")
+    print(f"Num texts no preprocessing: {text_no_preprocessing_cnt}")
+    print(chars_no_preprocessing)
+
+    print("----------Normalized characters----------")
+    char_list = sorted(list(chars_normalized.keys()))
+    print(f"Num normalized characters: {len(char_list)}")
+    print(char_list)
+
+    print("----------Durations----------")
+    print(f"Mean: {np.mean(durations)}, std: {np.std(durations)}")
+    print(f"Min: {np.min(durations)}, max: {np.max(durations)}")
+    print(f"99 percentile: {np.quantile(durations, 0.99)}")
+    print(f"95 percentile: {np.quantile(durations, 0.95)}")
+    print(f"90 percentile: {np.quantile(durations, 0.90)}")
+    print(f"10 percentile: {np.quantile(durations, 0.10)}")
+    print(f"5 percentile: {np.quantile(durations, 0.05)}")
+    print(f"1 percentile: {np.quantile(durations, 0.01)}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file-path", type=Path, required=True)
+    args = parser.parse_args()
+
+    inspect_dataset(args.file_path)
