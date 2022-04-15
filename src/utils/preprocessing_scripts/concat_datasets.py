e8f29600a144402bd9568aaad9d56db193a87a29
maevdokimov <maximevdfox@gmail.com>
2022-04-15 13:07:47 +0000

    feat: :wrench: refactored dataset concatenation

--- src/utils/preprocessing_scripts/concat_datasets.py
+++ src/utils/preprocessing_scripts/concat_datasets.py
@@ -16,18 +16,37 @@ def concat_datasets(first_path: Path, second_path: Path, out_path: Path):
             d = json.loads(line)
             second_lines.append(d)
 
-    if "speaker" in first_lines[0].keys():
-        num_speakers = len(set([d["speaker"] for d in first_lines]))
-        print(f"Num speakers: {num_speakers}")
+    first_has_speaker = "speaker" in first_lines[0].keys()
+    second_has_speaker = "speaker" in second_lines[0].keys()
 
+    first_num_speakers, second_num_speakers = None, None
+    if first_has_speaker:
+        first_num_speakers = len(set([d["speaker"] for d in first_lines]))
+        print(f"First num speakers: {first_num_speakers}")
+
+    if second_has_speaker:
+        second_num_speakers = len(set([d["speaker"] for d in second_lines]))
+        print(f"Second num speakers: {second_num_speakers}")
+
+    if not first_has_speaker and not second_has_speaker:
+        for d in first_lines:
+            d["speaker"] = 0
         for d in second_lines:
-            d["speaker"] = num_speakers
+            d["speaker"] = 1
+    elif first_has_speaker and not second_has_speaker:
+        for d in second_lines:
+            d["speaker"] = first_num_speakers
+    elif first_has_speaker and second_has_speaker:
+        for d in second_lines:
+            d["speaker"] += first_num_speakers
 
     with open(out_path, "w") as out_file:
         for d in first_lines:
             out_file.write(f"{json.dumps(d)}\n")
         for d in second_lines:
             out_file.write(f"{json.dumps(d)}\n")
+
+    return first_num_speakers, second_num_speakers
 
 
 if __name__ == "__main__":
