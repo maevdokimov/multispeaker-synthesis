7e9c350c447a1b67d1c3771810816d7316c7516f
maevdokimov <maximevdfox@gmail.com>
2022-04-18 17:38:22 +0000

    feat: :sparkles: vae loss

--- src/losses/tacotron2_vae_loss.py
+++ src/losses/tacotron2_vae_loss.py
@@ -1,7 +1,8 @@
+import math
+
 import torch
-from nemo.collections.tts.helpers.helpers import get_mask_from_lengths
 from nemo.core.classes import Loss, typecheck
-from nemo.core.neural_types.elements import LengthsType, LogitsType, LossType, MelSpectrogramType
+from nemo.core.neural_types.elements import LossType, NormalDistributionLogVarianceType, NormalDistributionMeanType
 from nemo.core.neural_types.neural_type import NeuralType
 
 
@@ -22,19 +23,32 @@ class Tacotron2VAELoss(Loss):
         self.x0 = anneal_x0
         self.upper = anneal_upper
 
-    def kl_anneal_function(self, anneal_function, lag, step, k, x0, upper):
-        if anneal_function == "logistic":
-            return upper / (upper + torch.exp(-k * (step - x0)))
-        elif anneal_function == "linear":
-            if step > lag:
-                return min(upper, step / x0)
+    def kl_anneal_function(self, step):
+        if self.anneal_function == "logistic":
+            return self.upper / (self.upper + math.exp(-self.k * (step - self.x0)))
+        elif self.anneal_function == "linear":
+            if step > self.lag:
+                return min(self.upper, step / self.x0)
             else:
                 return 0
-        elif anneal_function == "constant":
+        elif self.anneal_function == "constant":
             return 0.001
 
-    def forward(self, mu, logvar, step):
-        kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        kl_weight = self.kl_anneal_function(self.anneal_function, self.lag, step, self.k, self.x0, self.upper)
+    @property
+    def input_types(self):
+        return {
+            "mu": NeuralType(("B", "D"), NormalDistributionMeanType()),
+            "logvar": NeuralType(("B", "D"), NormalDistributionLogVarianceType()),
+        }
 
-        return kl_loss, kl_weight
+    @property
+    def output_types(self):
+        return {
+            "kl_loss": NeuralType(elements_type=LossType()),
+        }
+
+    @typecheck()
+    def forward(self, mu, logvar):
+        kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+        return kl_loss
