3a0629775bc3ad396a6f43b28c86f52a004c99bd
maevdokimov <maximevdfox@gmail.com>
2022-05-09 13:31:27 +0000

    feat: :sparkles: add weight loading from chekpoint

--- src/train/tacotron2_multispeaker.py
+++ src/train/tacotron2_multispeaker.py
@@ -1,4 +1,5 @@
 import pytorch_lightning as pl
+import torch
 from nemo.collections.common.callbacks import LogEpochTimeCallback
 from nemo.core.config import hydra_runner
 from nemo.utils.exp_manager import exp_manager
@@ -6,12 +7,15 @@ from nemo.utils.exp_manager import exp_manager
 from src.models.tacotron2_multispeaker import Tacotron2Multispeaker
 
 
-@hydra_runner(config_path="conf", config_name="tacotron2_multispeaker")
+@hydra_runner(config_path="conf", config_name="tacotron2_multispeaker_22050_finetune")
 def main(cfg):
     trainer = pl.Trainer(**cfg.trainer)
     exp_manager(trainer, cfg.get("exp_manager", None))
 
     model = Tacotron2Multispeaker(cfg=cfg.model, trainer=trainer)
+    if cfg.checkpoint_path is not None:
+        d = torch.load(cfg.checkpoint_path)
+        model.load_state_dict(d["state_dict"])
 
     lr_logger = pl.callbacks.LearningRateMonitor()
     epoch_time_logger = LogEpochTimeCallback()
